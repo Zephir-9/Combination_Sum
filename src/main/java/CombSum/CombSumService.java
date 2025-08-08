5fc0485dfd791768b6915cbf60a0ad40a8d07dd2
Zephyr <frastxini@gmail.com>
2025-08-08 20:25:21 +0300

    Initial commit

---
+++ src/main/java/CombSum/CombSumService.java
@@ -0,0 +1,7 @@
+import java.util.List;
+
+public class CombSumService {
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        return null;
+    }
+}
---
+++ src/main/java/CombSum/Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    static CombSumService combSumService;
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Number of candidates: ");
+        int size = input.nextInt();
+        int[] candidates = new int[size];
+
+        System.out.print("Enter " + size + " candidates");
+        for(int i = 0; i<size; i++) {
+            System.out.print("Enter element " + (i + 1) + ": ");
+            candidates[i] = input.nextInt();
+        }
+
+        System.out.print("Enter target number: ");
+        int target = input.nextInt();
+
+        System.out.print(combSumService.combinationSum2(candidates, target));
+    }
+}
